1ff640da5cbdcfa5fe274e0749224beedb42f2da
= <=>
2023-11-20 01:16:59 +0800

    rebuilding desaind not done make over

---
+++ asset/js/index.js
@@ -0,0 +1,23 @@
+var typed = new Typed(".typing", {
+  strings: [
+    "Maid Caffe",
+    "メイドカフェ",
+    "Make Fiture With Maids",
+    "メイドと未来を創る",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
+
+var typed = new Typed(".typing1", {
+  strings: [
+    "Maid Caffe",
+    "メイドカフェ",
+    "Make Fiture With Maids",
+    "メイドと未来を創る",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
